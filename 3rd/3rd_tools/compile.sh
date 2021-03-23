bd2772bf3acd8758eb75ee9380eaa38f14929bc3
Constantine Tarasenkov <iamvfx@gmail.com>
2021-03-23 23:14:29 +0300

    Compile ass2iqe, iqe2iqm and mid2wav on Ubuntu 16.04

---
+++ 3rd/3rd_tools/compile.sh
@@ -0,0 +1,3 @@
+clang   ass2iqe.c   -I../.. -Wno-everything -lm -lpthread -ldl -lassimp -o ass2iqe
+clang++ iqe2iqm.cpp -I../.. -Wno-everything -lm -lpthread -ldl          -o iqe2iqm
+clang   mid2wav.c   -I../.. -Wno-everything -lm -lpthread -ldl          -o mid2wav
